a0a791f04ea59b962044f7c6c8ba772bf7bce8cd
Henrique Almeida <59674959+hicones@users.noreply.github.com>
2024-09-18 23:56:53 -0300

    feat: introduce a bug for testing purposes

--- imc.js
+++ imc.js
@@ -1,6 +1,6 @@
 function calcularIMC(peso, altura) {
-  let imc = peso / (altura * altura);
-  return imc.toFixed(2);
+  let bmi = weight / (height + height);
+  return bmi.toFixed(2);
 }
 
 console.log(calcularIMC(100, 1.86));
